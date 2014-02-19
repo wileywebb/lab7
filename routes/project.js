86594839fec01024dd47962073fccd7fad3587eb
Wiley Webb <wwebb@stanford.edu>
2014-02-19 23:11:55 +0000

    done

--- routes/project.js
+++ routes/project.js
@@ -3,6 +3,10 @@ var models = require('../models');
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
+  models.Project
+    .find({ "_id": projectID})
+    .sort('-date')
+    .exec(afterQuery);
   // query for the specific project and
   // call the following callback
 
@@ -16,6 +20,22 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
+  var createProject = new models.Project({
+    "title": form_data["title"],
+    "date": form_data["date"],
+    "summary": form_data["summary"],
+    "image": form_data["image_url"]
+  });
+
+  // var createProject = new models.Project({form_data});
+
+   createProject.save(afterSave);
+
+    function afterSave(err) {
+      if (err) {console.log(err); res.send(500); }
+      console.log("saved");
+      res.redirect('/');
+    }
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
@@ -25,4 +45,13 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+  models.Project
+    .find({ "_id": projectID})
+    .remove()
+    .exec(afterRemove);
+
+    function afterRemove(err) {
+      if (err) {console.log(err); res.send(500); }
+    }
+ 
 }
